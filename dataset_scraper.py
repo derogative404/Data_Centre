ff576c5f4e5845d207349c5ed29c39d9fef76e76
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-02-21 16:44:17 +1300

    Updated gitignore and environment file to store link

--- dataset_scraper.py
+++ dataset_scraper.py
@@ -4,10 +4,17 @@ from pathlib import Path
 from zipfile import ZipFile
 import io
 import os
+import shutil
+from dotenv import load_dotenv
 
-xml_data = requests.get("https://divvy-tripdata.s3.amazonaws.com/").content
+# loading environment_variables
+load_dotenv()
+
+# preparing to scrape file data from XML file
+xml_data = requests.get(os.getenv('XML_LINK')).content
 soup = BeautifulSoup(xml_data, "xml")
-# Find the tag/child
+
+# Find the Key XML tag that contains the names of the latest files
 raw_files = soup.findAll("Key")
 files = dict()
 
@@ -15,7 +22,7 @@ for file in raw_files:
     if file.text.startswith("Divvy"):
         break
     else:
-        files.update({str(file.text).strip(): 'https://divvy-tripdata.s3.amazonaws.com/' + str(file.text).strip()})
+        files.update({str(file.text).strip(): os.getenv('XML_LINK') + str(file.text).strip()})
 
 #grabbing latest 12 filenames
 data_files = dict(sorted(files.items(), reverse=True)[:12])
@@ -26,6 +33,7 @@ for file_name in data_files.keys():
         r = requests.get(data_files.get(file_name))
         with ZipFile(io.BytesIO(r.content)) as f:
             f.extractall(Path("google_datasets"))
+            os.rename(Path("google_datasets/" + f.namelist()[0]), Path("google_datasets/" + file_name.replace("zip", "csv")))
         f.close()
 
 #removing files that do not exist in data_files list
@@ -33,3 +41,7 @@ for file_name in os.listdir("google_datasets"):
     if file_name.replace("csv", "zip") not in data_files.keys() and os.path.isdir(Path("google_datasets/" + file_name)) == False:
         path = Path("google_datasets/" + file_name)
         Path.unlink(path, missing_ok=True)
+    elif os.path.isdir(Path("google_datasets/" + file_name)) == True:
+        shutil.rmtree(Path("google_datasets/" + file_name))
+    else:
+        pass
